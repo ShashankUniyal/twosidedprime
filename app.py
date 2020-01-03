0f479b1782785e3473d4009c0f34416dbd1c4433
shashank_uniyal <shashank.uniyal@stagrp.com>
2020-01-03 13:04:35 +0530

    change code

--- app.py
+++ app.py
@@ -1,63 +1,13 @@
 from flask import Flask, jsonify, request
 import Utility
-import math
 
 app = Flask(__name__)
-
-def isprime(num):
-    no = int(num)
-    if no <= 1:
-        return False
-    elif (no == 2) or (no == 3):
-        return True
-    elif (no % 2 == 0) or (no % 3 == 0):
-        return False
-    else:
-        for i in range(2, math.floor(no / 2)):
-            if no % i == 0:
-                return False
-            continue
-        return True
-
-
-def twosidedprime(number):
-    # num = input('Enter the number:')
-    num = str(number)
-    num_length = len(num)
-    print(num_length);
-    if num_length == 1:
-        return isprime(num)
-    else:
-        print('')
-        return isprime(num) and truncateleft(num, num_length) and truncateright(num, num_length)
-
-
-def truncateleft(num, num_length):
-    if num_length == 1:
-        return isprime(num)
-    i = num[1: num_length + 1]
-    return isprime(i) and truncateleft(i, len(i))
-
-
-def truncateright(num, num_length):
-    if num_length == 1:
-        return isprime(num)
-    j = num[0: num_length - 1]
-    return isprime(j) and truncateright(j, len(j))
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    data = "hello world"
-    return jsonify({'data': data})
-
 
 @app.route("/doublesidedprime/<input>")
 def doublesidedprime(input):
     try:
-        num = int(input)
-        result = twosidedprime(num)
+        num = round(float(input))
+        result = Utility.twosidedprime(num)
         print(result)
         return {"StatusCode": "200", "Result": str(result)}
     except:
